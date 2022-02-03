2e5f1be7a3590628f315d39f1af3a19eeb19d489
Julian Baumgartner <jbaumgartner93@gmail.com>
2022-02-03 12:57:18 -0500

    bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyStrong",
-    version="0.0.4",
+    version="0.0.5",
     author="Julian Baumgartner",
     author_email="jbaumgartner93@gmail.com",
     description="Type enforcement for python objects.",
